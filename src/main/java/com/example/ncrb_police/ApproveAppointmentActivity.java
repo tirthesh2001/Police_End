8d3c0d67ebfead40db5bc34acb58462e3fa6feea
tirthesh2001 <tirthesh18@gmail.com>
2021-08-11 14:24:20 +0530

    Changes to beats and add appointment activity

---
+++ src/main/java/com/example/ncrb_police/ApproveAppointmentActivity.java
@@ -0,0 +1,14 @@
+package com.example.ncrb_police;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ApproveAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approve_appointment);
+    }
+}
